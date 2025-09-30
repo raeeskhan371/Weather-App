afcd8a2833f91e21f1bde5aa4e0947f9036c0a7c
raeeskhan371 <raeeskhann371@gmail.com>
2025-09-30 13:12:42 +0500

    add Files

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Activities/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  )
+  )
+}
